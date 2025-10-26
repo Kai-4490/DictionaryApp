5657375936f981966a9af948c5ecf2b36a610ac7
Kai-4490 <kakanivitthal@gmail.com>
2025-10-26 21:25:30 +0530

    k

--- DictionaryApp/src/Main.java
+++ DictionaryApp/src/Main.java
@@ -3,7 +3,6 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         String url = "jdbc:sqlite:database.db"; // SQLite database file
 
